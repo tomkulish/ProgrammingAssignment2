926783fee0d6d47c841915f144fbc604de587d9f
tkulish <thomas.kulish@gmail.com>
2015-01-23 19:17:08 -0500

    Initial function changes.

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,16 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseX <- NULL
+  set <- function(y) {
+    x <<- y
+    invserseX <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) inverseX <<- solve
+  getInverse <- function() inverseX
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverseX <- x$getInverse()
+    if(!is.null(inverseX)) {
+      message("getting cached data")
+      return(inverseX)
+    }
+    theMatrix <- x$get()
+    inverseX <- solve(theMatrix, ...)
+    x$setInverse(inverseX)
+    inverseX
 }
